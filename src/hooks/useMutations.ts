2f737935a019d91261bd3b756fa3cdc8bb40e57f
Dmitrii <rakhmanin.dm@gmail.com>
2024-09-26 23:02:14 +0300

    feat: add get users query invalidaton after user creation

--- src/hooks/useMutations.ts
+++ src/hooks/useMutations.ts
@@ -1,16 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import usersApi from "../api/user";
 import { TUserCreateDTO } from "../types/user";
+import { GET_USERS_QUERY_KEY } from "./useQueries";
 
 export const useAddUserMutation = (
-  onSuccess?: (data: unknown) => void,
+  onSuccess?: (data?: unknown) => void,
   onError?: () => void
 ) => {
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: (user: TUserCreateDTO) => {
       return usersApi.addUser(user);
     },
-    onSuccess,
+    onSuccess: () => {
+      onSuccess?.();
+      queryClient.invalidateQueries({ queryKey: [GET_USERS_QUERY_KEY] });
+    },
     onError,
   });
 
--- src/hooks/useQueries.ts
+++ src/hooks/useQueries.ts
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import usersApi from "../api/user";
 
+export const GET_USERS_QUERY_KEY = "users/get";
+
 export const useGetUsersQuery = () => {
   const { data, isLoading, error } = useQuery({
-    queryKey: ["users"],
+    queryKey: [GET_USERS_QUERY_KEY],
     queryFn: () => usersApi.getAllUsers(),
   });
   return {
