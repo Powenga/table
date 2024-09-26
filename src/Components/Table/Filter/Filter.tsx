f1d2983974f3e83465ccbe60e5cba232c787bfcd
Dmitrii <rakhmanin.dm@gmail.com>
2024-09-27 01:51:41 +0300

    ref: add not null condition to sort unique value in filters

--- src/Components/Table/Filter/Filter.tsx
+++ src/Components/Table/Filter/Filter.tsx
@@ -20,9 +20,9 @@ const Filter = <T,>(props: Props<T>) => {
 
   const sortedUniqueValues = useMemo(
     () =>
-      Array.from(column.getFacetedUniqueValues().keys()).sort((a, b) =>
-        a.localeCompare(b, "ru")
-      ),
+      Array.from(column.getFacetedUniqueValues().keys())
+        .filter((value) => value !== null)
+        .sort((a, b) => a?.localeCompare(b, "ru")),
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [column.getFacetedUniqueValues()]
