f69cb652b6630382e8ec97051e7e2a048778d2ee
Dmitrii <rakhmanin.dm@gmail.com>
2024-09-29 20:16:20 +0300

    ref: move App to app folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Users from "./Components/Users/Users";
+import Users from "../Components/Users/Users";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { indigo } from "@mui/material/colors";
